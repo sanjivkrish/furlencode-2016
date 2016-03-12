1c44521d49bb5a429b111553db75b4b20dc52e76
sanjivkrish <sanjiv436.k@gmail.com>
2016-03-12 19:19:02 +0530

    client: main: Add functions to support get directions
    Signed-off-by: sanjivkrish <sanjiv436.k@gmail.com>

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -52,4 +52,50 @@ angular.module('nightOwlApp')
 	          console.log('error in foursquare API');
 	      });
   };
+
+  //
+  // Gets the number and returns the array of same length
+  //
+  $scope.getNumber = function (rating) {
+		return new Array(parseInt(parseInt(rating)/2));
+  };
+
+  //
+  // Returns whether input is float or not
+  //
+  $scope.isRatingFloat = function (n) {
+		return ((n === +n) && (n !== (n|0)));
+  };
+
+  //
+  // Populate a map on showing directions
+  //
+	$scope.getDirection = function (venue) {
+		$scope.initMap(venue.location.lat, venue.location.lng);
+	};
+
+  $scope.initMap = function (lat, lng) {
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    var map = new google.maps.Map(document.getElementById('map-canvas'), {
+      zoom: 15,
+      center: {lat: lat, lng: lng}
+    });
+    directionsDisplay.setMap(map);
+    calculateAndDisplayRoute(directionsService, directionsDisplay, lat, lng);
+  }
+
+  function calculateAndDisplayRoute(directionsService, directionsDisplay, lat, lng) {
+    directionsService.route({
+      origin: {lat: $scope.userPlace.geometry.location.lat(), lng: $scope.userPlace.geometry.location.lng()},
+      destination: {lat: lat, lng: lng},
+      travelMode: google.maps.TravelMode.DRIVING
+    }, function(response, status) {
+      if (status === google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
+  }
 });
