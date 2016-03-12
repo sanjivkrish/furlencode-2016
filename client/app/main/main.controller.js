7e9fe05cfdb49a105301bacc31927d7a62408acf
sanjivkrish <sanjiv436.k@gmail.com>
2016-03-12 14:57:37 +0530

    client: main: Add a onChange listener to search box
    Signed-off-by: sanjivkrish <sanjiv436.k@gmail.com>

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,6 +1,20 @@
 'use strict';
 
 angular.module('nightOwlApp')
-  .controller('MainCtrl', function () {
+.controller('MainCtrl', function ($scope) {
+	//
+	// Watch function to search box
+	//
+	$scope.changePlace = function () {
+		if ($scope.userPlace.id) {
+			$scope.placeChanged();
+		}
+	};
 
-  });
+	//
+	// User switched to new place
+	//
+	$scope.placeChanged = function () {
+		console.log($scope.userPlace.id);
+	};
+});
