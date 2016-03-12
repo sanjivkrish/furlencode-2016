15f66fde24174843a28e107d7a76420943b0b072
sanjivkrish <sanjiv436.k@gmail.com>
2016-03-12 12:58:46 +0530

    Initial template created using yo
    command :
    yo angular-fullstack Night-owl
    
    ? What would you like to write scripts with? (Use arrow keys)
    ? What would you like to write scripts with? JavaScript
    ? Would you like to use Javascript ES6 in your client by preprocessing it with n
    ? What Angular router would you like to use? ngRoute
    ? Would you like to include Bootstrap? (Y/n) y
    ? Would you like to include Bootstrap? Yes
    ? Would you like to include UI Bootstrap? (Y/n) y
    ? Would you like to include UI Bootstrap? Yes
    
    Signed-off-by: sanjivkrish <sanjiv436.k@gmail.com>

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/nightowl-dev'
+  },
+
+  seedDB: true
+};
