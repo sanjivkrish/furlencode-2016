b529373137b730c93400be4423100ea574625fdc
sanjivkrish <sanjiv436.k@gmail.com>
2016-03-12 16:33:36 +0530

    client: main: Change UI
    Signed-off-by: sanjivkrish <sanjiv436.k@gmail.com>

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('nightOwlApp')
-.controller('MainCtrl', function ($scope) {
+.controller('MainCtrl', function ($scope, $location, locationInfo) {
 	//
 	// Watch function to search box
 	//
@@ -15,6 +15,25 @@ angular.module('nightOwlApp')
 	// User switched to new place
 	//
 	$scope.placeChanged = function () {
-		console.log($scope.userPlace.id);
+		console.log($scope.userPlace);
+		locationInfo.setUserPlace($scope.userPlace);
+		$scope.initializeMap($scope.userPlace.geometry.location.lat(), $scope.userPlace.geometry.location.lng(), 15);
 	};
+
+	//
+  //Passing Latitude, longitude and Zoom value as an argument to get that location
+  //
+  $scope.initializeMap = function (lat, lng, zoomValue) {
+      var myCenter = new google.maps.LatLng(lat, lng);
+      var mapProp = {
+          center: myCenter,
+          zoom: zoomValue,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      var map = new google.maps.Map(document.getElementById('map-canvas'), mapProp);
+      var marker = new google.maps.Marker({
+          position: myCenter
+      });
+      marker.setMap(map);
+  };
 });
