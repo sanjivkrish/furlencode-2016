82508adfcbf863965342c7f0b40b80836f1bbc75
sanjivkrish <sanjiv436.k@gmail.com>
2016-03-13 08:56:35 +0530

    server: rating: Implement the endpoint
    Signed-off-by: sanjivkrish <sanjiv436.k@gmail.com>

--- server/api/rating/rating.controller.js
+++ server/api/rating/rating.controller.js
@@ -1,8 +1,19 @@
 'use strict';
 
 var _ = require('lodash');
-
+var fs = require('fs');
+var count = 1;
 // Get list of ratings
 exports.index = function(req, res) {
-  res.json([]);
+	var obj = {
+		userId: req.body.user.data.id,
+		user: req.body.user.data.name,
+		rating: req.body.data.value,
+		comment: req.body.data.comment
+	};
+	fs.writeFile(count + '.json', JSON.stringify(obj), function () {
+		console.log('File created');
+		count += 1;
+	});
+  	res.json([]);
 };
